61c2d18066246b0a1dbaf2b5e1b5de170f8d0ff8
kavyansh <wadhwakavyansh2008@gmail.com>
2026-08-16 12:19:50 +0530

    structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="kavyansh",
+    author_email="wadhwakavyansh2008@gmail.com",
+    package=find_packages()
+)
